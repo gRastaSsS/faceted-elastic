422533875f5a2c8337d87cdc88cb19df49097eca
Vladislav Gerasimov <vladislav.gerasimov@genestack.com>
2021-06-28 18:16:54 +0300

    Add partition key to depth first

--- src/main/scala/generators/JsonGenerator.scala
+++ src/main/scala/generators/JsonGenerator.scala
@@ -13,120 +13,129 @@ object JsonGenerator {
     val mapper = new ObjectMapper()
     mapper.registerModule(DefaultScalaModule)
 
-    val studySize = args(1).toInt
-    val patientsPerStudy = 100
-    val samplesPerPatient = 100
-    val data = getData(studySize, patientsPerStudy, samplesPerPatient, args(2))
-
-    new PrintWriter(path) {
-      for (entity <- data) {
-        val jsonBody = mapper.writeValueAsString(entity)
-        write(jsonBody + "\n")
-      }
-      close()
-    }
-  }
-
-  private def getData(studySize: Int, patientsPerStudy: Int, samplesPerPatient: Int, order: String): Seq[Map[String, Any]] = {
-    val patientSize = studySize * patientsPerStudy
-    val sampleSize = studySize * patientsPerStudy * samplesPerPatient
+    val description = Map("study" -> args(1).toInt, "patient" -> 100, "sample" -> 100)
+    val order = args(2)
+    val writer = new PrintWriter(path)
 
     order match {
-      case "depth-first" =>
-        getDepthFirstData(Map(
-          "study" -> studySize,
-          "patient" -> patientsPerStudy,
-          "sample" -> samplesPerPatient
-        ))
-      case "breadth-first" =>
-        generateStudies(studySize) ++ generatePatients(patientSize, patientsPerStudy) ++ generateSamples(sampleSize, samplesPerPatient)
+      case "depth-first" => writeDepthFirst(description, writer, mapper)
+      case "breadth-first" => writeBreadthFirst(description, writer, mapper)
     }
   }
 
-  private def getDepthFirstData(levelSizes: Map[String, Int]): Stream[Map[String, Any]] = {
+  private def writeBreadthFirst(description: Map[String, Int], writer: PrintWriter, mapper: ObjectMapper): Unit = {
+    try {
+      val patientSize = description("study") * description("patient")
+      val sampleSize = description("study") * description("patient") * description("sample")
 
-    def gen(level: String, levelSize: Int, parentId: Int = 0): Stream[Map[String, Any]] = {
+      val data = (generateStudies(description("study"))
+        ++ generatePatients(patientSize, description("patient"))
+        ++ generateSamples(sampleSize, description("sample")))
+
+      data.foreach(e => {
+        val jsonBody = mapper.writeValueAsString(e)
+        writer.write(jsonBody + "\n")
+      })
+    } finally {
+      writer.close()
+    }
+  }
+
+  private def writeDepthFirst(description: Map[String, Int], writer: PrintWriter, mapper: ObjectMapper): Unit = {
+    try {
+      (0 until description("study"))
+        .foreach(studyId => {
+          val study = generateDepthFirstStudy(studyId, description)
+          study.foreach(data => {
+            val jsonBody = mapper.writeValueAsString(data)
+            writer.write(jsonBody + "\n")
+          })
+        })
+
+    } finally {
+      writer.close()
+    }
+  }
+
+  private def generateDepthFirstStudy(id: Int, levelSizes: Map[String, Int], usePartitionKey: Boolean = true): Stream[Map[String, Any]] = {
+    val partition = if (usePartitionKey) id else -1
+    val study = generateEntity("study", id, partition = partition)
+
+    def gen(level: String, levelSize: Int, parentId: Int, partition: Int = -1): Stream[Map[String, Any]] = {
       level match {
-        case "study" =>
-          if (levelSize == levelSizes("study")) Stream.empty
-          else (
-            generateStudy(levelSize)
-              #:: gen("patient", 0, parentId = levelSize)
-              #::: gen("study", levelSize + 1)
-            )
         case "patient" =>
           if (levelSize == levelSizes("patient")) Stream.empty
           else {
             val id = parentId * levelSizes("patient") + levelSize
-            (generatePatient(id, parentId)
-              #:: gen("sample", 0, parentId = id)
-              #::: gen("patient", levelSize + 1, parentId)
-              )
+            (generateEntity("patient", id, parentId, partition = partition)
+              #:: gen("sample", 0, parentId = id, partition = partition)
+              #::: gen("patient", levelSize + 1, parentId, partition = partition))
           }
+
         case "sample" =>
           if (levelSize == levelSizes("sample")) Stream.empty
           else {
             val id = parentId * levelSizes("sample") + levelSize
-            (generateSample(id, parentId)
-              #:: gen("sample", levelSize + 1, parentId)
-              )
+            (generateEntity("sample", id, parentId, partition = partition)
+              #:: gen("sample", levelSize + 1, parentId, partition = partition))
           }
       }
     }
 
-    gen("study", 0)
+    study #:: gen("patient", 0, parentId = id, partition = partition)
   }
 
   private def generateStudies(size: Int): Seq[Map[String, Any]] = {
     (0 until size by 1)
-      .map(id => generateStudy(id))
+      .map(id => generateEntity("study", id))
   }
 
   private def generatePatients(size: Int, perParent: Int): Seq[Map[String, Any]] = {
     (0 until size by 1)
-      .map(id => generatePatient(id, id / perParent))
+      .map(id => generateEntity("patient", id, id / perParent))
   }
 
   private def generateSamples(size: Int, perParent: Int): Seq[Map[String, Any]] = {
     (0 until size by 1)
-      .map(id => generateSample(id, id / perParent))
+      .map(id => generateEntity("sample", id, id / perParent))
   }
 
-  private def generateStudy(id: Int): Map[String, Any] = {
-    val countryValues = Seq("UK", "US", "Germany", "Japan", "Russia")
-    val typeValues = Seq("clinical", "other")
+  private def generateEntity(level: String, id: Int, parentId: Int = -1, partition: Int = -1): Map[String, Any] = {
+    val data = level match {
+      case "study" =>
+        val countryValues = Seq("UK", "US", "Germany", "Japan", "Russia")
+        val typeValues = Seq("clinical", "other")
+        Map(
+          "_table" -> "study",
+          "_id" -> id,
+          "country" -> countryValues(Random.nextInt(countryValues.length)),
+          "type" -> typeValues(Random.nextInt(typeValues.length))
+        )
 
-    Map(
-      "_table" -> "study",
-      "_id" -> id,
-      "country" -> countryValues(Random.nextInt(countryValues.length)),
-      "type" -> typeValues(Random.nextInt(typeValues.length))
-    )
-  }
+      case "patient" =>
+        val sexValues = Seq("male", "female")
+        Map(
+          "_table" -> "patient",
+          "_id" -> id,
+          "_parentId" -> parentId,
+          "age" -> Random.nextInt(80),
+          "sex" -> sexValues(Random.nextInt(sexValues.length))
+        )
 
-  private def generatePatient(id: Int, parentId: Int): Map[String, Any] = {
-    val sexValues = Seq("male", "female")
+      case "sample" =>
+        val typeValues = Seq("Type_1", "Type_2", "Type_3")
+        val organValues = Seq("Liver", "Lung", "Heart", "Brain")
+        Map(
+          "_table" -> "sample",
+          "_id" -> id,
+          "_parentId" -> parentId,
+          "type" -> typeValues(Random.nextInt(typeValues.length)),
+          "organ" -> organValues(Random.nextInt(organValues.length))
+        )
+    }
 
-    Map(
-      "_table" -> "patient",
-      "_id" -> id,
-      "_parentId" -> parentId,
-      "age" -> Random.nextInt(80),
-      "sex" -> sexValues(Random.nextInt(sexValues.length))
-    )
-  }
-
-  private def generateSample(id: Int, parentId: Int): Map[String, Any] = {
-    val typeValues = Seq("Type_1", "Type_2", "Type_3")
-    val organValues = Seq("Liver", "Lung", "Heart", "Brain")
-
-    Map(
-      "_table" -> "sample",
-      "_id" -> id,
-      "_parentId" -> parentId,
-      "type" -> typeValues(Random.nextInt(typeValues.length)),
-      "organ" -> organValues(Random.nextInt(organValues.length))
-    )
+    if (partition < 0) data
+    else data + ("_partition" -> partition)
   }
 }
 
